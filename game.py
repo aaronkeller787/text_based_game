86a7318ad4752fd66a5076cbae0d9d2349cbf271
Aaron <aaron@desktop.lan>
2021-03-17 21:12:13 -0700

    Changed the name, added in a new line for visability

--- game.py
+++ game.py
@@ -11,16 +11,17 @@ def welcome():
 
     os.system('clear')
 
-    print('\t##################################################################################')
-    print('\t##############     W e l c o m e  t o  S p a c e  M a r i n e s     ##############')
-    print('\t##################################################################################')
+    print('\t############################################################################################')
+    print('\t  #################     W e l c o m e  t o  T i d a l  F o r c e     #################')
+    print('\t       ###########################################################################')
+
 
 
 
 if __name__=="__main__":
 
     welcome()
-
+    print('\n')
     new_player = Player(input('Please enter your character name: '), 10, 5, 1)
     new_player.player_stats.update({'Name': {new_player.char_name}, 'HP': {new_player.player_hp}, 'Power': {new_player.player_power},'Gold':{new_player.player_gold}})
     
