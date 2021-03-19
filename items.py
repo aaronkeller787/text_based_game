3ea2436b2e13fa85858d3daddf9f1aa1ab4873eb
Aaron <aaron@desktop.lan>
2021-03-18 20:51:54 -0700

    Created starting weapon subclasses for each character

--- items.py
+++ items.py
@@ -1,3 +1,4 @@
+# Class for items inside of the game
 class Item():
     
     def __init__(self, name, description, value):
@@ -5,7 +6,7 @@ class Item():
         self.description = description
         self.value = value
 
-    
+# Stores the attributes for weapons, inherits from the items class
 class Weapon(Item):
 
     def __init__(self, name, description, value, damage):
@@ -16,7 +17,40 @@ class Weapon(Item):
         return "\n{}\n=====\n{}\nValue: {}\nDamage: {}".format(self.name, self.description, self.value, self.damage)
 
 
+# Will be starting weapon for the Fighter Class
+class Sword(Weapon):
 
-if __name__=="__main__":
-    new_item = Weapon('Bolter', 'A high damage pistol', 50, 10)
-    print(new_item)
+    def __init__(self):
+        self.name = 'Short Sword'
+        self.description = 'Small, lightweight, one-handed melee weapon'
+        self.value = 0
+        self.damage = 1
+
+# Will be starting weapon for the Cleric Class
+class Mace(Weapon):
+
+    def __init__(self):
+        self.name = 'Mace'
+        self.description = 'One handed, spiked mace'
+        self.value = 0
+        self.damage = 1
+
+# Will be starting weapon for the Mage Class
+class Wand(Weapon):
+
+    def __init__(self):
+        self.name = 'Wand'
+        self.description = 'Small, crooked, wodden wand.'
+        self.value = 0
+        self.damage = 1
+
+# Will be starting weapon for the Ranger Class
+class Bow(Weapon):
+
+    def __init__(self):
+        self.name = 'Bow'
+        self.description = 'Recurve bow'
+        self.value = 0
+        self.damage = 1
+
+
