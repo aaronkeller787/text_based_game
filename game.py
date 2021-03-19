3530b7d7cafc5bc827d356e8d00d6c3d12a87ba7
Aaron <aaron@desktop.lan>
2021-03-18 20:52:49 -0700

    Removed code for testing

--- game.py
+++ game.py
@@ -4,7 +4,7 @@ from os import system
 from time import sleep
 
 from dungeon import intro, room_one
-from player import Player
+from player import Player, choose_profession
 
 
 def welcome():
@@ -17,14 +17,12 @@ def welcome():
 
 
 
-
 if __name__=="__main__":
 
     welcome()
     print('\n')
-    new_player = Player(input('Please enter your character name: '), 10, 5, 1)
+    new_player = Player(input('Please enter your character name: '), 10, 5, 1, 1)
     new_player.player_stats.update({'Name': {new_player.char_name}, 'HP': {new_player.player_hp}, 'Power': {new_player.player_power},'Gold':{new_player.player_gold}})
-    
     
     room_one()
 
