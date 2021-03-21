f2c7f59d1b22ef4e6da868b2d535e5dbc39b787c
Aaron <aaron@desktop.lan>
2021-03-20 23:07:19 -0700

    Added in section to read in intro text from intro.txt

--- game.py
+++ game.py
@@ -5,6 +5,7 @@ from time import sleep
 
 from dungeon import intro, room_one
 from player import Player, choose_profession
+from program_functions import print_slow
 
 
 def welcome():
@@ -14,18 +15,15 @@ def welcome():
     print('\t############################################################################################')
     print('\t  #################     W e l c o m e  t o  T i d a l  F o r c e     #################')
     print('\t       ###########################################################################')
+    print("\n\n")
 
+    f = open("intro.txt", "r")
+    print_slow(f.read())
 
+    choose_profession()
 
 if __name__=="__main__":
-
     welcome()
-    print('\n')
-    new_player = Player(input('Please enter your character name: '), 10, 5, 1, 1)
-    new_player.player_stats.update({'Name': {new_player.char_name}, 'HP': {new_player.player_hp}, 'Power': {new_player.player_power},'Gold':{new_player.player_gold}})
-    
-    room_one()
-
 
    
 
