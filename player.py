570d7e56433aaa3c2d28c8589b76dddab3b0c4d6
Aaron <aaron@desktop.lan>
2021-03-20 23:08:18 -0700

    Added spacing for easier visibility

--- player.py
+++ player.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 import random
+import os
 from items import Sword, Mace, Wand, Bow
 
 class Player():
@@ -11,79 +12,84 @@ class Player():
         self.player_gold = player_gold
         self.player_exp = player_exp
 
-
+# Class attributes for the Fighter 
 class Fighter(Player):
 
+    os.system('clear')
     profession = 'Fighter'
     starting_weapon = Sword()
     class_description = "The figher is a warrior, and expert in weapons and, if he is clever, tactics and strategy."
         
     def __init__(self):
-        self.char_name = input('What is your character name? ')
+        self.char_name = input('What is your character name: ')
         self.player_hp = 15
         self.player_power = 1
         self.player_gold = 1
         self.player_exp = 1
 
-        print("{}\n=====\n{}\n".format(self.profession, self.class_description))
+        print("\n{}\n=====\n{}\n".format(self.profession, self.class_description))
         print(self.starting_weapon)
 
-
+# Class attributes for the Paladin 
 class Paladin(Player):
 
+    os.system('clear')
     profession = 'Paladin'
     starting_weapon = Mace()
     class_description = "The paladin is a noble and heroic warrior, the symbol of all that is right and true in the world."
         
     def __init__(self):
-        self.char_name = input('What is your character name? ')
+        self.char_name = input('What is your character name:  ')
         self.player_hp = 15
         self.player_power = 1
         self.player_gold = 1
         self.player_exp = 1
 
-        print("{}\n=====\n{}\n".format(self.profession, self.class_description))
+        print("\n{}\n=====\n{}\n".format(self.profession, self.class_description))
         print(self.starting_weapon)
 
-   
+# Class attributes for the Mage 
 class Mage(Player):
        
+    os.system('clear')
     profession = 'Mage'
     starting_weapon = Wand()
     class_description = "Mages are the most versitile types of Wizard, those who choose not to specialize in any single school of magic."
         
     def __init__(self):
-        self.char_name = input('What is your character name? ')
+        self.char_name = input('What is your character name:  ')
         self.player_hp = 15
         self.player_power = 1
         self.player_gold = 1
         self.player_exp = 1
 
-        print("{}\n=====\n{}\n".format(self.profession, self.class_description))
+        print("\n{}\n=====\n{}\n".format(self.profession, self.class_description))
         print(self.starting_weapon)
 
-
+# Class attributes for the Ranger 
 class Ranger(Player):
 
+    os.system('clear')
     profession = 'Ranger'
     starting_weapon = Bow()
     class_description = "The ranger is a hunter and woodsman who lives by not only his sword, but also his wits."
         
     def __init__(self):
-        self.char_name = input('What is your character name? ')
+        self.char_name = input('What is your character name:  ')
         self.player_hp = 15
         self.player_power = 1
         self.player_gold = 1
         self.player_exp = 1
 
-        print("{}\n=====\n{}\n".format(self.profession, self.class_description))
+        print("\n{}\n=====\n{}\n".format(self.profession, self.class_description))
         print(self.starting_weapon)
 
 
 def choose_profession():
+
     print("Which class would you like to play?",'\n',
           "Press f for Fighter",'\n',
-          "Press c for Paladin",'\n',
+          "Press p for Paladin",'\n',
           "Press m for Mage",'\n',
           "Press r for Ranger")
     print('\n')
