f205947ef17cbc321fd09fe8704595be07fb4211
Aaron <aaron@desktop.lan>
2021-03-25 20:18:04 -0700

    Moved necessary instance variables to class variables. Added in the HalfElf Race. Added in method for getting user input in the HalfElf Race class

--- races.py
+++ races.py
@@ -15,139 +15,151 @@ class Races():
         self.common_traits = common_traits
 
 ###### Halfling and Subraces ######
-
 class Halfling(Races):
 
     bonus_dexterity = 2
+    race_name = 'Halfling'
+    lifespan = 150
+    adulthood = 20
+    size = 'Small'
+    speed = 25
+    languages = ['Common','Halfling']
+    common_traits = ['Lucky', 'Brace', 'Halfling nimbleness']
 
     def __init__(self):
-        self.race_name = 'Halfling'
         self.race_weight = input('Please enter your weight, between 37 and 45: ')
-        self.lifespan = 150
-        self.adulthood = 20
-        self.size = 'Small'
-        self.speed = 25
-        self.languages = {'Language1': 'Common', 'Language2': 'Halfling'}
-        self.common_traits = ['Lucky', 'Brave', 'Halfling numbleness']
 
 class Lightfoot(Halfling):
 
     bonus_charisma = 1
     subrace_trait = ['Naturally Stealthy']
-    
-    def __init__(self):
-        self.race_name = 'Lightfoot'
-
+    race_name = 'Lightfoot'
 
 class Stout(Halfling):
 
     bonus_constitution = 1
-
-    def __init__(self,subrace_trait):
-        self.race_name = 'Stout'
-        self.subrace_trait = ['Stout Resilliance']
+    race_name = 'Stout'
+    subrace_trait = ['Stout Resilliance']
         
 ###### Gnome and Subraces ########
-
 class Gnome(Races):
     
     bonus_intelligence = 2
+    race_name = 'Gnome'
+    lifespan = 425
+    adulthood = 40
+    size = 'Small'
+    speed = 25
+    languages = ['Common', 'Gnomish']
+    common_traits = ['Dark Vision', 'Gnome Cunning']
 
     def __init__(self):
-        self.race_name = 'Gnome'
         self.race_weight = input('Please enter your weight, between 37 and 45: ')
-        self.lifespan = 425
-        self.adulthood = 40
-        self.size = 'Small'
-        self.speed = 25
-        self.languages = {'Language1': 'Common', 'Language2': 'Gnomish'}
-        self.common_traits = ['Dark Vision', 'Gnome Cunning']
-
+     
 class ForestGnome(Gnome):
 
+    race_name = 'Forest Gnome'
     bonus_dexterity = 1
-
-    def __init__(self,subrace_trait):
-        self.race_name = 'Forest Gnome'
-        self.subrace_trait = [bonus_dexterity,'Natural Illusionist','Speak with Small Beasts']
+    subrace_trait = ['Natural Illusionist','Speak with Small Beasts']
 
 class RockGnome(Gnome):
 
-    def __init__(self,bonus_constitution,subrace_trait):
-        self.race_name = 'Rock Gnome'
-        self.bonus_constitution = 1
-        self.subrace_trait = [bonus_constitution,'Artificer\'s Lore','Tinker']
+    race_name = 'Rock Gnome'
+    bonus_constitution = 1
+    subrace_trait = ['Artificer\'s Lore','Tinker']
 
 ###### Dwarf and Subraces ########
-
 class Dwarf(Races):
 
+    race_name = 'Dwarf'
+    lifespan = 350
+    adulthood = 50
+    size = 'Medium'
+    speed = 25
+    languages = {'Language1': 'Common', 'Language2': 'Dwarvish'}
+    bonus_constitution = 2
+    common_traits = ['Dark Vision', 'Dwarven Resiliance', 'Combat Training', 'Tool Proficiency', 'Stonecunning']
+
     def __init__(self,bonus_constitution):
-        self.race_name = 'Dwarf'
         self.race_weight = input('Please enter your weight, between 119 and 226: ')
-        self.lifespan = 350
-        self.adulthood = 50
-        self.size = 'Medium'
-        self.speed = 25
-        self.languages = {'Language1': 'Common', 'Language2': 'Dwarvish'}
-        self.bonus_constitution = 2
-        self.common_traits = ['Dark Vision', 'Dwarven Resiliance', 'Combat Training', 'Tool Proficiency', 'Stonecunning']
 
 class HillDwarf(Dwarf):
 
-    def __init__(self, bonus_wisdom, subrace_trait):
-        self.race_name = 'Hill Dwarf'
-        self.bonus_wisdom = 1
-        self.subrace_trait = [bonus_wisdom, 'Dwarven Toughness']
+    race_name = 'Hill Dwarf'
+    bonus_wisdom = 1
+    subrace_trait = ['Dwarven Toughness']
 
 class MountainDwarf(Dwarf):
 
-    def __init__(self, bonus_strength, subrace_trait):
-        self.race_name = 'Mountain Dwarf'
-        self.bonus_strength = 2
-        self.subrace_trait = [bonus_strength, 'Armor Training']
-
+    race_name = 'Mountain Dwarf'
+    bonus_strength = 2
+    subrace_trait = ['Armor Training']
 
 ###### Elf and Subraces ########
-
 class Elf(Races):
 
+    race_name = 'Elf'
     bonus_dexterity = 2
+    lifespan = 750
+    adulthood = 100
+    size = 'Medium'
+    speed = 30
+    languages = {'Language1': 'Common', 'Language2': 'Elvish'}
+    common_traits = ['Dark Vision', 'Keen Sense', 'Fey Ancestry', 'Trance']
 
     def __init__(self):
-        self.race_name = 'Elf'
         self.race_weight = input('Please enter your weight, between 79 and 180: ')
-        self.lifespan = 750
-        self.adulthood = 100
-        self.size = 'Medium'
-        self.speed = 30
-        self.languages = {'Language1': 'Common', 'Language2': 'Elvish'}
-        self.common_traits = ['Dark Vision', 'Keen Sense', 'Fey Ancestry', 'Trance']
-
-
+     
 class HighElf(Elf):
 
-    def __init__(self, bonus_intelligence, subrace_trait):
-        self.race_name = 'High Elf'
-        self.bonus_intelligence = 1
-        self.subrace_trait = [bonus_intelligence, 'Weapon Training', 'Cantrip', 'Extra Language']
-
+    race_name = 'High Elf'
+    bonus_intelligence = 1
+    subrace_trait = ['Weapon Training', 'Cantrip', 'Extra Language']
 
 class WoodElf(Elf):
 
-    def __init__(self, bonus_wisdom, subrace_trait):
-        self.race_name = 'Wood Elf'
-        self.bonus_wisdom = 1
-        self.subrace_trait = [bonus_wisdom, 'Weapon Training', 'Fleet of Foot', 'Mask of the Wild']
+    race_name = 'Wood Elf'
+    bonus_wisdom = 1
+    subrace_trait = ['Weapon Training', 'Fleet of Foot', 'Mask of the Wild']
 
 class DarkElf(Elf):
 
-    def __init__(self, bonus_charisma, subrace_trait):
-        self.race_name = 'Dark Elf'
-        self.bonus_charisma = 1
-        self.subrace_trait = [bonus_charisma, 'Weapon Training', 'Superior Darkvision', 'Sunlight Sensitivity', 'Drow Magic']
+    race_name = 'Dark Elf'
+    bonus_charisma = 1
+    subrace_trait = [bonus_charisma, 'Weapon Training', 'Superior Darkvision', 'Sunlight Sensitivity', 'Drow Magic']
+
+###### Half Elf ########
+class HalfElf(Races):
+
+    race_name = 'Half Elf'
+    bonus_charisma = 2
+    lifespan = 180
+    adulthood = 20
+    size = 'Medium'
+    speed = 30
+    languages = ['Common','Elvish']
+    common_traits = ['Darkvision', 'Fey Ancestry']
+    
+    
+    def __init__(self,third_language,third_skill,fourth_skill,bonus_attributes_one,bonus_attributes_two):
+        self.third_language = third_language
+        self.third_skill = third_skill
+        self.fourth_skill = fourth_skill
+        self.bonus_attributes_one = bonus_attributes_one
+        self.bonus_attributes_two = bonus_attributes_two
+
+    @classmethod
+    def get_values(values):
+        return values(
+        third_language = input('Please select a 3rd language: '),
+        third_skill = input('Please select a 3rd skill: '),
+        fourth_skill = input('Please select a 4th skill: '),
+        bonus_attributes_one = input('Please select a 2nd attribute: '),
+        bonus_attributes_two = input('Please select a 3rd attribute: '),
+        )
 
 
 if __name__=="__main__":
-    new_race = Lightfoot()
-    print(new_race.bonus_charisma)
+    new_race = HalfElf.get_values()
+    new_race.languages.append(new_race.third_language)
+    print(new_race.languages)
